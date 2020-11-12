2d5dfa0e1c47155f911ab8616c20d8f53a838820
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-11-11 22:53:18 -0500

    Add mongoose connection and dotenv config to servver.js

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
 const express = require("express");
-const path = require("path");
+const mongoose = require("mongoose");
+const routes = require("./routes")
+
+require('dotenv').config();
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+// Serve up static assets if in production (on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+// Add routes (both API and view)
+app.use(routes);
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+
+// Connect to MongoDB
+mongoose.connect(
+  process.env.MONGODB_URI || process.env.DB_LOCAL
+);
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
